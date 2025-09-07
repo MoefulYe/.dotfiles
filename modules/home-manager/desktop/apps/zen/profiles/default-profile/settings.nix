0f8a40a5f6d5e7ae8031f1d20861f39e1613592e
ashenye <luren145@gmail.com>
2025-09-07 10:10:48 +0800

    config video hardware accel for zen

--- modules/home-manager/desktop/apps/zen/profiles/default-profile/settings.nix
+++ modules/home-manager/desktop/apps/zen/profiles/default-profile/settings.nix
@@ -2,5 +2,9 @@
 {
   settings = {
     "browser.link.open_newwindow" = 1;
+    # https://wiki.archlinux.org/title/firefox#Hardware_video_acceleration
+    "media.ffmpeg.vaapi.enabled" = true;
+    "media.hardware-video-decoding.enabled" = true;
+    "media.hardware-video-decoding.force-enabled" = true;
   };
 }
