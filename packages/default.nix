d466d44f53e1483812bd385f345d1be098e22a41
ashenye <luren145@gmail.com>
2025-09-16 13:08:58 +0800

    x

--- packages/default.nix
+++ packages/default.nix
@@ -1,15 +1,15 @@
 { pkgs, ... }:
 {
   fonts = pkgs.callPackage ./my-fonts.nix {
-    url = "https://pub-074e0c0a63da4754aa4f0abb1851b769.r2.dev/fonts.zip";
+    url = "https://blob.desktop.nix.059867.xyz/fonts.zip";
     hash = "sha256-/1ryVz2fcIOftsUjPCuQI5zMmtNJE3g4AUf92Zwux8o=";
   };
   swaylock-background = pkgs.callPackage ./my-swaylock-background.nix {
-    url = "https://pub-074e0c0a63da4754aa4f0abb1851b769.r2.dev/swaylock.jpg";
+    url = "https://blob.desktop.nix.059867.xyz/swaylock.jpg";
     hash = "sha256-C5UuZeec+mGerebHffg+g8CNrOZd6OYG0LDnn56ldtE=";
   };
   wallpapers = pkgs.callPackage ./my-wallpapers.nix {
-    url = "https://pub-074e0c0a63da4754aa4f0abb1851b769.r2.dev/wallpapers.zip";
+    url = "https://blob.desktop.nix.059867.xyz/wallpapers.zip";
     hash = "sha256-3hS/mgfKweSP66JycLoxsa5I9Iojrn5gZWFAlFXnWfo=";
   };
   zju-connect = pkgs.callPackage ./zju-connect.nix { };
