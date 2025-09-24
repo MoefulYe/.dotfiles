9e52caa309c2d4045fab8ca5e6501d8c9c8d9071
ashenye <luren145@gmail.com>
2025-09-24 21:33:49 +0800

    x

--- inventory/external-hosts/ssh-configs/zju-lab-serv-zhang.nix
+++ inventory/external-hosts/ssh-configs/zju-lab-serv-zhang.nix
@@ -5,17 +5,26 @@
 }:
 {
   sops.secrets = {
-    LAB_SERV_ZHANG = {
+    ZHANG_IP = {
       mode = "0400";
       sopsFile = "${paths.secrets}/zju.yaml";
     };
+    ZHANG_PORT = {
+      mode = "0400";
+      sopsFile = "${paths.secrets}/zju.yaml";
+    };
+    ZHANG_USER = {
+      mode = "0400";
+      sopsFile = "${paths.secrets}/zju.yaml";
+    };
+
   };
 
   sops.templates."zju-lab-serv-zhang.conf".content = ''
     Host zju-lab-serv-zhang
-      HostName 10.22.81.253
-      User ubuntu
-      Port 14222
+      HostName ${config.sops.placeholder.ZHANG_IP}
+      User ${config.sops.placeholder.ZHANG_USER}
+      Port ${config.sops.placeholder.ZHANG_PORT}
   '';
   programs.ssh.includes = [
     config.sops.templates."zju-lab-serv-zhang.conf".path
