350cd3fa17d5b25bcb6eb995e080ced8da58d8d8
ashenye <luren145@gmail.com>
2025-09-25 12:55:37 +0800

    x

--- inventory/nixos-hosts/nixos-module-entries/desk00-u265kf-lan/users/ashenye.nix
+++ inventory/nixos-hosts/nixos-module-entries/desk00-u265kf-lan/users/ashenye.nix
@@ -8,5 +8,6 @@ in
     "${hmProfiles}/features/gaming/shadps4.nix"
     inventory.externalHosts.zju-lab-serv-w3090.sshConfig
     inventory.externalHosts.zju-lab-serv-zhang.sshConfig
+    inventory.externalHosts.zju-lab-serv-yu-sg.sshConfig
   ];
 }
--- inventory/nixos-hosts/nixos-module-entries/lap00-xiaoxin-mei/users/ashenye.nix
+++ inventory/nixos-hosts/nixos-module-entries/lap00-xiaoxin-mei/users/ashenye.nix
@@ -7,5 +7,6 @@ in
     "${hmRoles}/daily"
     inventory.externalHosts.zju-lab-serv-w3090.sshConfig
     inventory.externalHosts.zju-lab-serv-zhang.sshConfig
+    inventory.externalHosts.zju-lab-serv-yu-sg.sshConfig
   ];
 }
