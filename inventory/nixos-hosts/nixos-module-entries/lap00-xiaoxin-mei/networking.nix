4ac31b79a1f7b206e2cf6a65d31ab4b4e83f95fd
ashenye <luren145@gmail.com>
2025-09-26 13:09:22 +0800

    x

--- inventory/nixos-hosts/nixos-module-entries/lap00-xiaoxin-mei/networking.nix
+++ inventory/nixos-hosts/nixos-module-entries/lap00-xiaoxin-mei/networking.nix
@@ -1,50 +1,26 @@
 { config, paths, ... }:
 let
-  inherit (paths) osProfiles osModules;
-  zjuConnectSock5Port = 31080;
+  inherit (paths) osProfiles;
   proxyFwMark = 666;
   outbounds = [
     "wlp2s0"
   ];
   tproxyPort = 7895;
+  dnsPort = 7853;
 in
 {
   imports = [
     "${osProfiles}/features/networking/mihomo/presets/tproxy.nix"
-    "${osProfiles}/features/networking/nftables/presets/tproxy-zju.nix"
-    "${osModules}/services/zju-connect.nix"
+    "${osProfiles}/features/networking/nftables/presets/tproxy-v2.nix"
+    "${osProfiles}/features/networking/nftables/presets/sys-fw.nix"
+    "${osProfiles}/hardware/wireless.nix"
   ];
-  config = {
-    sops.secrets = {
-      STU_ID = {
-        mode = "0400";
-        sopsFile = "${paths.secrets}/zju.yaml";
-      };
-      STU_PASSWD = {
-        mode = "0400";
-        sopsFile = "${paths.secrets}/zju.yaml";
-      };
-    };
-    sops.templates."zju-connect.toml".content = ''
-      username = "${config.sops.placeholder.STU_ID}"
-      password = "${config.sops.placeholder.STU_PASSWD}"
-      http_bind = ""
-      socks_bind = ":${toString config.services.zju-connect.socks5Port}"
-    '';
-    services.zju-connect = {
-      socks5Port = zjuConnectSock5Port;
-      configPath = config.sops.templates."zju-connect.toml".path;
-    };
-    networking.nftables.presets.tproxy = {
-      inherit tproxyPort proxyFwMark outbounds;
-    };
-    services.mihomo.presets.tproxy = {
-      inherit tproxyPort;
-      routingMark = proxyFwMark;
-      zjuConnect = {
-        enable = true;
-        socks5Port = zjuConnectSock5Port;
-      };
-    };
+  networking.interfaces.wlp2s0.useDHCP = true;
+  networking.nftables.presets.tproxy-v2 = {
+    inherit tproxyPort dnsPort outbounds;
+  };
+  services.mihomo.presets.tproxy = {
+    inherit tproxyPort dnsPort;
+    zjuConnect.enable = false;
   };
 }
