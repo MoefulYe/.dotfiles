d8ed8ec7698cf15a121b2b9a1cae9822c4a72e64
ashenye <luren145@gmail.com>
2025-10-04 08:53:04 +0800

    x

--- profiles/hm/features/development/neovim/plugins/lsp/lsp.nix
+++ profiles/hm/features/development/neovim/plugins/lsp/lsp.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   programs.nixvim = {
     plugins = {
@@ -11,71 +11,6 @@
       lsp = {
         enable = true;
         inlayHints = true;
-        servers = {
-          # html = {
-          #   enable = true;
-          # };
-          # lua_ls = {
-          #   enable = true;
-          # };
-          # nil_ls = {
-          #   enable = true;
-          # };
-          # ts_ls = {
-          #   enable = true;
-          # };
-          # marksman = {
-          #   enable = true;
-          # };
-          # pyright = {
-          #   enable = true;
-          # };
-          # gopls = {
-          #   enable = true;
-          # };
-          # terraformls = {
-          #   enable = true;
-          # };
-          # ansiblels = {
-          #   enable = true;
-          # };
-          # jsonls = {
-          #   enable = true;
-          # };
-          # helm_ls = {
-          #   enable = true;
-          #   extraOptions = {
-          #     settings = {
-          #       "helm_ls" = {
-          #         yamlls = {
-          #           path = "${pkgs.yaml-language-server}/bin/yaml-language-server";
-          #         };
-          #       };
-          #     };
-          #   };
-          # };
-          # yamlls = {
-          #   enable = true;
-          #   extraOptions = {
-          #     settings = {
-          #       yaml = {
-          #         schemas = {
-          #           kubernetes = "'*.yaml";
-          #           "http://json.schemastore.org/github-workflow" = ".github/workflows/*";
-          #           "http://json.schemastore.org/github-action" = ".github/action.{yml,yaml}";
-          #           "http://json.schemastore.org/ansible-stable-2.9" = "roles/tasks/*.{yml,yaml}";
-          #           "http://json.schemastore.org/kustomization" = "kustomization.{yml,yaml}";
-          #           "http://json.schemastore.org/ansible-playbook" = "*play*.{yml,yaml}";
-          #           "http://json.schemastore.org/chart" = "Chart.{yml,yaml}";
-          #           "https://json.schemastore.org/dependabot-v2" = ".github/dependabot.{yml,yaml}";
-          #           "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" = "*docker-compose*.{yml,yaml}";
-          #           "https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json" = "*flow*.{yml,yaml}";
-          #         };
-          #       };
-          #     };
-          #   };
-          # };
-        };
 
         keymaps = {
           silent = true;
@@ -130,9 +65,9 @@
         };
       };
     };
-    extraPlugins = with pkgs.vimPlugins; [
-      ansible-vim
-    ];
+    # extraPlugins = with pkgs.vimPlugins; [
+    #   ansible-vim
+    # ];
 
     extraConfigLua = ''
             local _border = "rounded"
