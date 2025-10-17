b452815081e41053f157d6c769e3ba44ccc8ee44
ashenye <luren145@gmail.com>
2025-10-17 20:39:26 +0800

    x

--- helpers/splitFullyQualifiedUsername.nix
+++ helpers/splitFullyQualifiedUsername.nix
@@ -1,7 +1,7 @@
 #sgmeeser@name的完全限定用户名拆分为用户名与主机名
 { fullyQualifiedUserName, lib }:
 let
-  parts =  lib.splitString "@" fullyQualifiedUserName;
+  parts = lib.splitString "@" fullyQualifiedUserName;
   username = builtins.elemAt parts 0;
   hostname = builtins.elemAt parts 1;
 in
--- inventory/hm-users/default.nix
+++ inventory/hm-users/default.nix
@@ -1,12 +1,23 @@
 {
   "ashenye@desk00-u265kf-lan" = {
-    mainModule = ./hm-module-entries + "/ashenye@desk00-u265kf-lan.nix" ;
-    extraModules = [ 
+    mainModule = ./hm-module-entries + "/ashenye@desk00-u265kf-lan.nix";
+    extraModules = [
       ../../quirks/shared/unsafe-openssl.nix
       ../../quirks/shared/unsafe-libxml2-2.13.8.nix
     ];
     userInfo = {
       description = "ashenye on desk00-u265kf-lan";
+      tags = [ ];
+    };
+  };
+  "ashenye@lap00-xiaoxin-mei" = {
+    mainModule = ./hm-module-entries + "/ashenye@lap00-xiaoxin-mei.nix";
+    extraModules = [
+      ../../quirks/shared/unsafe-openssl.nix
+      ../../quirks/shared/unsafe-libxml2-2.13.8.nix
+    ];
+    userInfo = {
+      description = "ashenye on lap00-xiaoxin-mei";
       tags = [ ];
     };
   };
