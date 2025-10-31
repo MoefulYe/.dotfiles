d8dbdacbe4b788665ce9a3eb8783f2a0817665df
ashenye <luren145@gmail.com>
2025-10-31 21:12:30 +0800

    x

--- inventory/default.nix
+++ inventory/default.nix
@@ -1,11 +1,20 @@
-rec {
-  # 外部的不受自己管理的主机
-  externalHosts = import ./external-hosts;
-  # 内部的受自己管理的nixos主机
-  nixosHosts = import ./nixos-hosts;
-  # 内部的受自己管理的非nixos主机
-  nonNixosHosts = import ./non-nixos-hosts;
-  hosts = externalHosts // nixosHosts // nonNixosHosts;
-  # 使用home-manager管理的用户
-  hmUsers = import ./hm-users;
+{
+  hosts = rec {
+    # 外部的不受自己管理的主机
+    external = import ./hosts/external;
+    # 内部的受自己管理的nixos主机
+    nixos = import ./hosts/nixos;
+    # 内部的受自己管理的非nixos主机
+    non-nixos = import ./hosts/non-nixos;
+    all = external // nixos // non-nixos;
+  };
+  users = rec {
+    # 外部用户，不受自己管理的用户
+    external = import ./users/external;
+    # 内部的使用home-manager管理的用户
+    hm = import ./users/hm;
+    # 内部的非home-manager管理的用户
+    non-hm = import ./users/non-hm;
+    all = external // hm // non-hm;
+  };
 }
