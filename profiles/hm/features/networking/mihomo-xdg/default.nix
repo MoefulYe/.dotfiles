fb902dda8b97e625f881ede2c77e80c549532830
ashenye <luren145@gmail.com>
2025-11-09 13:13:45 +0800

    Fix Mihomo Web UI exec URL to point to the correct path

--- profiles/hm/features/networking/mihomo-xdg/default.nix
+++ profiles/hm/features/networking/mihomo-xdg/default.nix
@@ -7,7 +7,7 @@
       comment = "Open Mihomo Web UI in default browser";
       type = "Application";
       # Use default browser via xdg-open. Adjust URL if you host a local external-ui.
-      exec = "${pkgs.xdg-utils}/bin/xdg-open \"http://localhost:9090\"";
+      exec = "${pkgs.xdg-utils}/bin/xdg-open \"http://localhost:9090/ui\"";
       terminal = false;
       # Uses a common theme icon. Replace with a custom icon name or absolute path if desired.
       icon = "${./icon.png}";
