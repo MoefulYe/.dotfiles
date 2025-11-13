d33971384a6acdbde697b258adaaaa1c4d03f562
ashenye <luren145@gmail.com>
2025-11-13 18:57:16 +0800

    Uncomment Neovim configuration files in default.nix

--- profiles/hm/features/development/neovim/default.nix
+++ profiles/hm/features/development/neovim/default.nix
@@ -1,11 +1,11 @@
-{ inputs, pkgs, ... }:
+{ inputs, ... }:
 {
   imports = [
     inputs.nixvim.homeModules.nixvim
-    # ./settings.nix
-    # ./autocmds.nix
-    # ./keymaps.nix
-    # ./plugins
+    ./settings.nix
+    ./autocmds.nix
+    ./keymaps.nix
+    ./plugins
   ];
   programs.nixvim.enable = true;
 }
