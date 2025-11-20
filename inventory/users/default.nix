7bd29cd7cf3428e06d0956195a5107a41d483ac5
ashenye <luren145@gmail.com>
2025-11-20 21:46:40 +0800

    Add inventory structure and configuration for hosts and users
    - Introduced a new README.md for the inventory with detailed tiered structure.
    - Created README.md for hosts categorizing them into managed and unmanaged.
    - Added default.nix files for hosts with specific configurations.
    - Removed obsolete configurations for external hosts.
    - Implemented detailed configurations for two hosts: desk00-u265kf-lan and lap00-xiaoxin-mei.
    - Established user configurations with SSH settings and roles.
    - Added SSH configuration files for external users.
    - Removed deprecated user and host configurations.

---
+++ inventory/users/default.nix
@@ -0,0 +1,36 @@
+let
+  mkHmConfigEntry = username: ./hm-module-entries + "/" + username;
+in
+{
+  "ashenye@desk00-u265kf-lan" = {
+    description = "ashenye on desk00-u265kf-lan";
+    role = "cat";
+    tags = [
+      "gaming"
+    ];
+    sshPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOvnf1TDq7kpCwOMFK0Vn6x7zjMEiGGIVhknGN+kC3n0 ashenye@desk00-u265kf-lan";
+    hmConfig = mkHmConfigEntry "ashenye@desk00-u265kf-lan";
+  };
+  "ashenye@lap00-xiaoxin-mei" = {
+    description = "ashenye on lap00-xiaoxin-mei";
+    role = "cat";
+    tags = [ ];
+    sshPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINCHoR+QLELtWTjo8EWiat8FNcyiAEQniZ6kkaOFCIlV ashenye@lap00-xiaoxin-mei";
+    hmConfig = mkHmConfigEntry "ashenye@lap00-xiaoxin-mei";
+  };
+  "ubuntu@zju-zhang" = {
+    username = "ubuntu";
+    hostname = "sv";
+    role = "fox";
+    tags = [ "zju" ];
+    hmConfig = mkHmConfigEntry "ubuntu@zju-zhang";
+    sshConfig = ./ssh-configs/zju-zhang.nix;
+  };
+  "yu@zju-yu-sg" = {
+    username = "jiongchiyu";
+    hostname = "XXF-GPU-00";
+    role = "fox";
+    tags = [ "zju" ];
+    sshConfig = ./ssh-configs/zju-yu-sg.nix;
+  };
+}
