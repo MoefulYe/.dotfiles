179b2120c15e33bda34263ed53eddeff36f1976e
ashenye <luren145@gmail.com>
2025-11-20 22:38:58 +0800

    Fix SSH configuration by correcting sshPubKey variable names and updating authorized keys handling for user access.

--- inventory/topology/ssh.nix
+++ inventory/topology/ssh.nix
@@ -5,7 +5,7 @@ let
   mkCompleteGraph = nodes: mkCartesianProduct nodes nodes;
 in
 # 允许笔记本通过SSH访问台式机
-(mkEdge "ashenye@lap00-xiaoxin-mei" "ashenye@desk00-u265kf-lan")
+[ (mkEdge "ashenye@lap00-xiaoxin-mei" "ashenye@desk00-u265kf-lan") ]
 # 允许自己的主机访问浙江大学的两台服务器
 ++ (mkCartesianProduct
   [ "ashenye@desk00-u265kf-lan" "ashenye@lap00-xiaoxin-mei" ]
