ff679aaaa86b9368610c4538dda8bd590c4f218a
ashenye <luren145@gmail.com>
2025-11-30 10:13:32 +0800

    remove obsolete Nix configurations for zju and nixos hosts

--- inventory/users/ssh-configs/ubuntu@zju-zhang.nix
+++ inventory/users/ssh-configs/ubuntu@zju-zhang.nix
@@ -1,31 +1,7 @@
 {
-  config,
-  paths,
-  ...
-}:
-{
-  sops.secrets = {
-    ZHANG_IP = {
-      mode = "0400";
-      sopsFile = "${paths.secrets}/zju.yaml";
-    };
-    ZHANG_PORT = {
-      mode = "0400";
-      sopsFile = "${paths.secrets}/zju.yaml";
-    };
-    ZHANG_USER = {
-      mode = "0400";
-      sopsFile = "${paths.secrets}/zju.yaml";
-    };
+  programs.ssh.matchBlocks."zju-zhang" = {
+    hostname = "10.98.36.162";
+    user = "ubuntu";
+    port = 22;
   };
-
-  sops.templates."zju-zhang.conf".content = ''
-    Host zju-zhang
-      HostName ${config.sops.placeholder.ZHANG_IP}
-      User ${config.sops.placeholder.ZHANG_USER}
-      Port ${config.sops.placeholder.ZHANG_PORT}
-  '';
-  programs.ssh.includes = [
-    config.sops.templates."zju-zhang.conf".path
-  ];
 }
