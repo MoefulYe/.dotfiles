8a6304b7eedfa307f3f438e02dbdd265a35f838f
ashenye <luren145@gmail.com>
2025-12-06 17:31:08 +0800

    feat: update treesitter grammar packages to include specific languages

--- profiles/hm/features/development/neovim/plugins/editor/treesitter.nix
+++ profiles/hm/features/development/neovim/plugins/editor/treesitter.nix
@@ -8,7 +8,86 @@
     };
     folding = false;
     nixvimInjections = true;
-    grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
+    grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+      astro
+      awk
+      bash
+      c
+      cmake
+      comment
+      cpp
+      css
+      csv
+      cuda
+      diff
+      dockerfile
+      git_config
+      git_rebase
+      gitattributes
+      gitcommit
+      gitignore
+      glsl
+      goctl
+      godot_resource
+      gomod
+      gosum
+      gotmpl
+      gowork
+      graphql
+      haskell
+      haskell_persistent
+      helm
+      hlsl
+      html
+      http
+      ini
+      java
+      javadoc
+      javascript
+      jq
+      jsdoc
+      json
+      json5
+      jsonc
+      jsonnet
+      just
+      kconfig
+      kdl
+      kitty
+      llvm
+      lua
+      luadoc
+      luap
+      luau
+      markdown
+      markdown_inline
+      mermaid
+      meson
+      mlir
+      nginx
+      ninja
+      nix
+      objc
+      objdump
+      proto
+      python
+      regex
+      rust
+      scss
+      slint
+      sql
+      ssh_config
+      toml
+      tsx
+      typescript
+      typespec
+      typoscript
+      vim
+      vimdoc
+      vue
+      yaml
+      zsh
+    ];
   };
 
   programs.nixvim.plugins.treesitter-textobjects = {
