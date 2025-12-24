c64de35ac44e68c54c3278d3cf9d2636e0c57da1
ashenye <luren145@gmail.com>
2025-12-24 22:26:32 +0800

    feat: enhance Tailscale configuration with routing features and firewall settings

--- inventory/hosts/nixos-module-entries/rutr01-j4105-qingloong/tailscale.nix
+++ inventory/hosts/nixos-module-entries/rutr01-j4105-qingloong/tailscale.nix
@@ -4,5 +4,11 @@
   services.tailscale.extraUpFlags = [
     "--advertise-routes=192.168.231.0/24"
     "--accept-dns=false"
+    "--advertise-exit-node"
   ];
+  services.tailscale.useRoutingFeatures = "server";
+  networking.firewall = {
+    allowedUDPPorts = [ 41641 ]; # 允许直连端口
+    trustedInterfaces = [ "tailscale0" ];
+  };
 }
