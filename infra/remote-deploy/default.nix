94678929bcdb2fdc8b399863b8ecd7f323347b9b
ashenye <luren145@gmail.com>
2025-12-24 18:37:36 +0800

    x

--- infra/remote-deploy/default.nix
+++ infra/remote-deploy/default.nix
@@ -8,33 +8,34 @@ let
 in
 {
   nodes = {
-    # desk00-u265kf-lan = {
-    #   sshUser = "ashenye";
-    #   hostname = "lan.void";
-    #   profiles = {
-    #     system = {
-    #       user = "root";
-    #       path = deploy-rs.lib.x86_64-linux.activate.nixos outputs.nixosConfigurations.desk00-u265kf-lan;
-    #     };
-    #     ashenye = {
-    #       user = "ashenye";
-    #       path =
-    #         deploy-rs.lib.x86_64-linux.activate.home-manager
-    #           outputs.homeConfigurations."ashenye@desk00-u265kf-lan";
-    #     };
-    #   };
-    # };
+    desk00-u265kf-lan = {
+      hostname = "lan.void";
+      profiles = {
+        system = {
+          sshUser = "deployee";
+          user = "root";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos outputs.nixosConfigurations.desk00-u265kf-lan;
+        };
+        ashenye = {
+          sshUser = "ashenye";
+          user = "ashenye";
+          path =
+            deploy-rs.lib.x86_64-linux.activate.home-manager
+              outputs.homeConfigurations."ashenye@desk00-u265kf-lan";
+        };
+      };
+    };
     lap00-xiaoxin-mei = {
-      sshUser = "ashenye";
       hostname = "mei.void";
       profiles = {
         system = {
           user = "root";
+          sshUser = "deployee";
           path = deploy-rs.lib.x86_64-linux.activate.nixos outputs.nixosConfigurations.lap00-xiaoxin-mei;
-          interactiveSudo = true;
         };
         ashenye = {
           user = "ashenye";
+	  sshUser = "ashenye";
           path =
             deploy-rs.lib.x86_64-linux.activate.home-manager
               outputs.homeConfigurations."ashenye@lap00-xiaoxin-mei";
@@ -47,11 +48,12 @@ in
       remoteBuild = true;
       profiles = {
         system = {
+          sshUser = "deployee";
           user = "root";
           path = deploy-rs.lib.aarch64-darwin.activate.darwin outputs.darwinConfigurations.lap01-macm4-mume;
-          interactiveSudo = true;
         };
       };
     };
   };
+  sshOpts = [ "-p" "2222" ];
 }
