6448606a7765a99f3ff7fa1107e1894ece4ef7f2
ashenye <luren145@gmail.com>
2025-12-25 22:45:53 +0800

    x

--- infra/remote-deploy/deployee.nix
+++ infra/remote-deploy/deployee.nix
@@ -9,6 +9,11 @@
         hashedPassword = "$y$j9T$t/Cvl6/8QfmSpf59G7sxt1$HO6ltOhL5Fs3HGyJ..FCRbtfWWCICoiCuUk9SmL.jsC";
     };
   } else {
+    deployee = {
+        openssh.authorizedKeys.keys = [
+          (builtins.readFile ./id_ed25519.pub)
+        ];
+    };
   }; 
   security.sudo.extraConfig = ''
     deployee ALL=(ALL) NOPASSWD: ALL
