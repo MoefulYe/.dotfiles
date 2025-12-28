ce4ee070176f5e5fe66265122725c50ca3343e93
ashenye <luren145@gmail.com>
2025-12-28 13:37:05 +0800

    feat: enhance networking configuration by adding bridge interface and updating USB interface settings

--- inventory/hosts/nixos-module-entries/lap00-xiaoxin-mei/networking.nix
+++ inventory/hosts/nixos-module-entries/lap00-xiaoxin-mei/networking.nix
@@ -3,14 +3,34 @@
   ...
 }:
 let
-  interface = "enp5s0f3u1";
+  usbInterface = "enp5s0f3u1";
+  bridgeInterface = "br0";
 in
 {
   imports = [
     (inventory.topology.networks.void.nixosConfig.staticMemberNetworkdConfig {
-      inherit interface;
+      interface = bridgeInterface;
     })
   ];
+  systemd.network.netdevs."20-${bridgeInterface}" = {
+    netdevConfig = {
+      Name = bridgeInterface;
+      Kind = "bridge";
+    };
+  };
+  systemd.network.networks."10-${usbInterface}" = {
+    matchConfig.Name = usbInterface;
+    networkConfig = {
+      Bridge = bridgeInterface;
+    };
+  };
+  systemd.network.networks."30-${bridgeInterface}" = {
+    matchConfig.Name = bridgeInterface;
+    networkConfig = {
+      DHCP = "no";
+      IPv6AcceptRA = false;
+    };
+  };
   systemd.network.networks.wlp2s0 = {
     enable = true;
     matchConfig.Name = "wlp2s0";
