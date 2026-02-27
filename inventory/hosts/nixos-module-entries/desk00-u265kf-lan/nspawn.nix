2228902275f03f320e739099bff5d75d733e20f0
ashenye <luren145@gmail.com>
2026-02-27 23:04:47 +0800

    add nspawn

--- inventory/hosts/nixos-module-entries/desk00-u265kf-lan/nspawn.nix
+++ inventory/hosts/nixos-module-entries/desk00-u265kf-lan/nspawn.nix
@@ -1,33 +1,50 @@
 { config, lib, ... }:
 {
-  virtualisation.systemd-nspawn.enable = true;
-
   systemd.nspawn."ubuntu-ml" = {
-    enable = true;
-    execConfig.Boot = true;
-    networkConfig.VirtualEthernet = false;
-
-    bind = [
-      "/home/ashenye/repo:/repo"
-    ];
-
-    bindRW = [
-      "/dev/nvidia0"
-      "/dev/nvidiactl"
-      "/dev/nvidia-modeset"
-      "/dev/nvidia-uvm"
-      "/dev/nvidia-uvm-tools"
-      "/dev/dri"
-    ];
-
+    networkConfig = {
+      MACVLAN = "enp131s0:eth0";
+    };
+    execConfig = {
+      Boot = true;
+      Environment = [
+        "LD_LIBRARY_PATH=/run/opengl-driver/lib:/run/opengl-driver-32/lib"
+        "PATH=/run/current-system/sw/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+      ];
+      Hostname = "ubuntu-ml";
+      ResolvConf = "bind-host";
+    };
     filesConfig = {
-      RootDirectory = "/var/lib/machines/ubuntu-ml";
+      Bind = [
+        "/dev/nvidia0"
+        "/dev/nvidiactl"
+        "/dev/nvidia-modeset"
+        "/dev/nvidia-uvm"
+        "/dev/nvidia-uvm-tools"
+        "/dev/dri"
+      ];
+      BindReadOnly = [
+        "/run/current-system/sw"
+        "/run/opengl-driver"
+        "/run/opengl-driver-32"
+        "/nix"
+      ];
     };
   };
 
-  systemd.services."systemd-nspawn@ubuntu-ml".wantedBy = [ "multi-user.target" ];
-
-  systemd.tmpfiles.rules = [
-    "d /var/lib/machines/ubuntu-ml 0755 root root -"
-  ];
+  systemd.services."systemd-nspawn@ubuntu-ml" = {
+    overrideStrategy = "asDropin";
+    serviceConfig = {
+      DevicePolicy = "closed";
+      DeviceAllow = [
+        "/dev/nvidia0 rw"
+        "/dev/nvidiactl rw"
+        "/dev/nvidia-modeset rw"
+        "/dev/nvidia-uvm rw"
+        "/dev/nvidia-uvm-tools rw"
+        "/dev/dri/card1 rw"
+        "/dev/dri/renderD128 rw"
+      ];
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
 }
