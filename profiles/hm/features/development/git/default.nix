91ed4140a160f67c898ff482e6ffadfb33fcccb0
ashenye <luren145@gmail.com>
2026-03-23 18:49:28 +0800

    x

--- profiles/hm/features/development/git/default.nix
+++ profiles/hm/features/development/git/default.nix
@@ -1,10 +1,30 @@
 { me, ... }:
 {
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      side-by-side = true;
+      line-numbers = true;
+      navigate = true;
+      syntax-theme = "Nord";
+    };
+  };
+
   programs.git = {
     enable = true;
     settings = {
       user = {
         inherit (me) name email;
+      };
+      merge = {
+        conflictStyle = "zdiff3";
+      };
+      pull = {
+        rebase = true;
+      };
+      rebase = {
+        autoStash = true;
       };
     };
   };
