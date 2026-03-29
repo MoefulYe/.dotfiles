65a558fe824646f3286eda455e9a5b0985eb06a8
ashenye <luren145@gmail.com>
2026-03-29 16:24:23 +0800

    不要让通知抢焦点

--- profiles/hm/features/desktop/swaync.nix
+++ profiles/hm/features/desktop/swaync.nix
@@ -4,6 +4,14 @@
     # https://github.com/ErikReider/SwayNotificationCenter/blob/main/src/configSchema.json
     # see gist.github.com/JannisPetschenka/fb00eec3efea9c7fff8c38a01ce5d507
     settings = {
+      # Keep notifications visible but avoid overlay-level focus behavior.
+      layer = "top";
+      "control-center-layer" = "top";
+
+      # Disable keyboard-interactive features to reduce focus stealing.
+      "keyboard-shortcuts" = false;
+      "notification-inline-replies" = false;
+      "layer-shell-cover-screen" = false;
     };
 
   };
