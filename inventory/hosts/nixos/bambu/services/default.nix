cbc2d816f471c5865e577ee4f994eb283bd88ed9
ashenye <luren145@gmail.com>
2026-04-18 22:15:34 +0800

    x

--- inventory/hosts/nixos/bambu/services/default.nix
+++ inventory/hosts/nixos/bambu/services/default.nix
@@ -24,17 +24,8 @@
 
   services.nginx = {
     enable = true;
-    recommendedProxySettings = true;
     recommendedGzipSettings = true;
     recommendedOptimisation = true;
-
-    virtualHosts."_" = {
-      default = true;
-      rejectSSL = true;
-      extraConfig = ''
-        return 444;
-      '';
-    };
   };
 
   networking.firewall.allowedTCPPorts = [
