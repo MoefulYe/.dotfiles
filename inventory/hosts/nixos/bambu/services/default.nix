ddb31954fc1c8f2e10dac478dd4745b01ca99678
ashenye <luren145@gmail.com>
2026-04-18 20:33:54 +0800

    添加服务

--- inventory/hosts/nixos/bambu/services/default.nix
+++ inventory/hosts/nixos/bambu/services/default.nix
@@ -1,5 +1,13 @@
 {
   virtualisation.docker.enable = true;
   users.users.ashenye.extraGroups = [ "docker" ];
-
+  services.qbittorrent = {
+    enable = true;
+    openFirewall = true;
+    webuiPort = 38081;
+  };
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+  };
 }
